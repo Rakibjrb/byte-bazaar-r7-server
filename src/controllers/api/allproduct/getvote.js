77d583f846b6f6cc44805f200d8b291d518e7645
rakibjrb <rakibjrb572157@gmail.com>
2023-11-26 23:36:45 +0600

    updated getvotes function

--- src/controllers/api/allproduct/getvote.js
+++ src/controllers/api/allproduct/getvote.js
@@ -3,7 +3,10 @@ const Votes = require("../../../models/allproduct/votes");
 const getVotes = async (req, res, next) => {
   const getvotedata = req.query;
   try {
-    const vote = await Votes.findOne({ productId: getvotedata.productId });
+    const vote = await Votes.findOne({
+      productId: getvotedata.productId,
+      useremail: getvotedata.email,
+    });
     res.send(vote);
   } catch (error) {
     next(error);
