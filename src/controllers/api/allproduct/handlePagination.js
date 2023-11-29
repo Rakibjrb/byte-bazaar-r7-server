61e8bae3385d7b3a4351373e3e958d1f803ee526
rakibjrb <rakibjrb572157@gmail.com>
2023-11-29 23:04:59 +0600

    fixed handlePagination function approved data finding

--- src/controllers/api/allproduct/handlePagination.js
+++ src/controllers/api/allproduct/handlePagination.js
@@ -4,7 +4,9 @@ const handlePagination = async (req, res, next) => {
   const skip = req.query.skip;
   const limit = req.query.limit;
   try {
-    const paginationData = await AllProduct.find().skip(skip).limit(limit);
+    const paginationData = await AllProduct.find({ status: "Approved" })
+      .skip(skip)
+      .limit(limit);
     res.send(paginationData);
   } catch (error) {
     next(error);
